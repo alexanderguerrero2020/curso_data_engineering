6c1f06b9b40b976d6d1f8ca56feff38f739b56b5
EVERTH ALEXANDER LOPEZ <alexguerrerolopez2000@gmail.com>
2024-06-06 09:00:36 +0000

    Update

--- analyses/codegen.sql
+++ analyses/codegen.sql
@@ -1,3 +1,3 @@
 {{ codegen.generate_model_yaml(
-    model_names=['stg_sql_server_dbo__ADDRESSES', 'stg_sql_server_dbo__EVENTS', 'stg_sql_server_dbo__EVENT_TYPE', 'stg_sql_server_dbo__ORDERS','stg_sql_server_dbo__ORDER_ITEMS','stg_sql_server_dbo__PRODUCTS','stg_sql_server_dbo__PROMOS','stg_sql_server_dbo__SHIPPING_SERVICE','stg_sql_server_dbo__STATUS_ORDERS','stg_sql_server_dbo__STATUS_PROMOS','stg_sql_server_dbo__USERS']
+    model_names=['int__pedidos']
 ) }}
